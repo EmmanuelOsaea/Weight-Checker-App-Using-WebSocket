5531ea7e663687ec3ca24175b007dcd70ef3ff5e
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-31 12:04:12 +0100

    Create WeightViewModel.kt

---
+++ WeightViewModel.kt
@@ -0,0 +1,11 @@
+package com.emmaxtech.weightchecker.viewmodel
+
+import androidx.lifecycle.Viewmodel
+import androidx.lifecycle.viewModelScope
+import com.emmaxtech.weightchecker.data.WeightRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+
+
