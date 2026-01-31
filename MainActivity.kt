3e795e2b37ba7b32cc255568dc1dae02fcc50542
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-31 13:18:55 +0100

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,14 @@
+package com.emmaxtech.weightchecker
+
+import androidx.activity
+import androidx.activity
+import com.emmaxtech.weightchecker.ui.WeightInputScreen
+
+class MainActivity
+ override fun onCreate(savedInstanceState: Bundle?) {
+   super.onCreate(savedInstanceState)
+   setContent {
+     WeightInputScreen
+   }
+ }
+}
