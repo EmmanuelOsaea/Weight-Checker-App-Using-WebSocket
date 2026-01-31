411eec9930f3359a1f60d25daf9908f450eec07c
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-31 12:29:40 +0100

    Update WeightViewModel.kt

--- WeightViewModel.kt
+++ WeightViewModel.kt
@@ -7,5 +7,25 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+class WeightViewModel {
+priva
+  te val repository: WeightRepository = WeightRepository()
+) : ViewModel() {
 
+  private val _weight = MutableStateFlow("")
+  val weight: StateFlow<String> = _weight
+
+  fun onWeightChanged(newWeight: String) {
+    _weight.value = newWeight
+  }
+
+  fun saveWeight() {
+    val weightValue = _weight.value.toFloatorNull()
+    if (weightValue ! = null) {
+      viewModelScope.launch {
+        repository.saveWeight(weightValue)
+      }
+    }
+  }
+}
 
