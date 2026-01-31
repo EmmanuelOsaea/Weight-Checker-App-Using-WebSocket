876ec131a5b1710b00047e34bc59f665e91c43b6
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-31 11:54:30 +0100

    Create WeightRepository.kt

---
+++ WeightRepository.kt
@@ -0,0 +1,11 @@
+package com.emmaxtech.weightchecker.data
+
+class WeightRepository {
+
+  fun saveWeight(weight: Float) {
+  }
+  
+ fun getWeightHistory(): List <Float> {
+   return listOf()
+}
+}
